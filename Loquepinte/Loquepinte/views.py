1d083c48891fffb5f283001c19841be41a5cd741
Luis Peralta <luis_peralta_000@hotmail.com>
2020-08-31 18:15:09 -0300

    iniciamos en Django

---
+++ Loquepinte/Loquepinte/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.template import Template, Context
+
+def home(request):
+    doc_externo=open("C:/Users/Luis/Desktop/Aplicacion/Loquepinte/Loquepinte/plantillas/index.html")
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({"nombre_persona"})
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
+
+def principal(request):
+    doc_externo=open("C:/Users/Luis/Desktop/Aplicacion/Loquepinte/Loquepinte/plantillas/principal.html")
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({"nombre_persona"})
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
